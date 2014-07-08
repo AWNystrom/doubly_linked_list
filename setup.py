36361ae178f7695c593c84e0947271fa68474289
Andrew Nystrom <awnystrom@gmail.com>
2014-07-08 11:50:57 -0500

    packaging files added

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name = "doubly_linked_list",
+    version = "1.0.0",
+    description = "Doubly linked list datastructure in Python",
+    author = "Andrew Nystrom",
+    author_email = "AWNystrom@gmail.com",
+    url = "https://github.com/AWNystrom/doubly_linked_list",
+    keywords = ["doubly", "linked", "list", "doublylinkedlist"],
+    classifiers = ["Programming Language :: Python", 
+    			   "Programming Language :: Python :: 2.7",
+    			   "Programming Language :: Python :: 2",
+    			   "License :: OSI Approved :: Apache Software License",
+    			   "Operating System :: OS Independent",
+    			   "Development Status :: 4 - Beta",
+    			   "Intended Audience :: Developers"
+    			   ]
+	)
